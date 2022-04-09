52a4ea8b6bad91a82b8d60d2815c1d9cfd0eeb13
David Strömbäck <david.e.stromback@gmail.com>
2022-04-09 20:17:42 +0200

    include position in interpolation tokens

--- src/interpolate.ts
+++ src/interpolate.ts
@@ -8,7 +8,12 @@ export function* interpolate<T, S>(
   let result = yield* tokenize(strings[0]);
 
   for (let index = 0; index < values.length; index++) {
-    yield { type: "interpolation", value: values[index] };
+    yield {
+      type: "interpolation",
+      value: values[index],
+      start: result.offset,
+      end: result.offset,
+    };
 
     result = yield* tokenize(strings[index + 1], result);
   }
